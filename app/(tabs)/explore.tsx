35ff18e5d61eb5474444d41c691546ff86caa8b6
dstjkw <sattarkhuzhaevadariga@gmail.com>
2024-07-29 17:54:53 +0500

    App change

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -13,9 +13,9 @@ export default function TabTwoScreen() {
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Library</ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
+      <ThemedText>This is changed.</ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
           This app has two screens:{' '}
